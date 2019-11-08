901cb10c4bbcc7f544dedb51ab988a2431d14392
Vinay Arora <Vinay.Arora@aristocrat.com>
2019-11-08 16:34:50 +0530

    Advanced register your own Aggregators

---
+++ src/Bucket.App/Bucket.WebGetway/Aggregators/FakeDefinedAggregator.cs
@@ -0,0 +1,43 @@
+﻿using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucket.WebGetway.Aggregators
+{
+    public class FakeDefinedAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamContext> responses)
+        {
+            var contentBuilder = new StringBuilder();
+            contentBuilder.Append("{");
+            foreach (var response in responses)
+            {
+                var responseContent = await response.DownstreamResponse.Content.ReadAsStringAsync();
+
+                contentBuilder.AppendFormat("\"{0}\":{1}", response.DownstreamReRoute.Key, responseContent);
+                contentBuilder.Append(",");
+            }
+
+            var responseClub = contentBuilder.ToString();
+            if (responseClub.EndsWith(","))
+            {
+                responseClub = responseClub.Remove(responseClub.Length - 1);
+            }
+            contentBuilder = new StringBuilder(responseClub);
+            contentBuilder.Append("}");
+            var stringContent = new StringContent(contentBuilder.ToString())
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), HttpStatusCode.OK.ToString());
+        }
+    }
+}
--- src/Bucket.App/Bucket.WebGetway/Startup.cs
+++ src/Bucket.App/Bucket.WebGetway/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bucket.WebGetway.Aggregators;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,7 +29,8 @@ namespace Bucket.WebGetway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddOcelot(Configuration);
+            services.AddOcelot(Configuration)
+                .AddSingletonDefinedAggregator<FakeDefinedAggregator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
